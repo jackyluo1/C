98a8a75f60ec3ea83627d41b74e716e1a1b5340a
luoj976 <78027883+luoj976@users.noreply.github.com>
2022-04-29 19:44:32 -0700

    Delete Lab4_Linkers directory

--- Lab4_Linkers/hack.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-// Our version of the random number generator
-int TopSecretGenerateRandomNumber(){
- 	return 42;
- 	}
-// Write your hack.c file here and compile it into a shared library(.so)
